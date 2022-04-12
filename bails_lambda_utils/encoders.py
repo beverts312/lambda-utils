bb44999f117bb7d231307216fe258dbb6d26254a
Bailey Everts <texas312@gmail.com>
2022-04-12 03:36:46 +0000

    fix: remove uneeded method

--- bails_lambda_utils/encoders.py
+++ bails_lambda_utils/encoders.py
@@ -4,27 +4,15 @@ import re
 
 
 class ModelEncoder(json.JSONEncoder):
+    """
+    Maps Pynamo models and other json to a json format lambda's can process
+    """
+
     def default(self, obj):
         if hasattr(obj, "attribute_values"):
-            return ModelEncoder.transform_object(
-                obj.attribute_values,
-                obj.get_legacy_fields()
-                if hasattr(obj, "get_legacy_fields")
-                else {},
-            )
+            return obj.attribute_values
         elif isinstance(obj, datetime.datetime):
             return obj.isoformat()
         elif isinstance(obj, set):
             return list(obj)
         return json.JSONEncoder.default(self, obj)
-
-    @staticmethod
-    def transform_object(obj, extra_attributes):
-        new_obj = {}
-        for key in obj:
-            new_obj[ModelEncoder.transform_key(key)] = obj[key]
-        return {**new_obj, **extra_attributes}
-
-    @staticmethod
-    def transform_key(key):
-        return re.sub(r"(?!^)_([a-zA-Z])", lambda m: m.group(1).upper(), key)
